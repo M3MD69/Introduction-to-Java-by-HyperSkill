0e387dca61fb95a12556d15ddcfcd769b53ec8f6
مُحَمَّد سَعْدَاوِيْ <m3md69@outlook.com>
2025-04-10 20:48:31 +0200

    # Learn (Array) Topic

---
+++ Topics/Array/Initializing an array of characters/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        final char[] CHARACTERS = {'a', 'z', 'e', 'd'};
+        System.out.println(Arrays.toString(CHARACTERS));
+    }
+}
