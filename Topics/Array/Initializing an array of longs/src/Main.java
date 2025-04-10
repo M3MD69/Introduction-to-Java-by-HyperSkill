c923be0348f9e9966c4932fc53550a69c6ecb39e
مُحَمَّد سَعْدَاوِيْ <m3md69@outlook.com>
2025-04-10 20:48:31 +0200

    # Learn and Solve (Array) Topic

---
+++ Topics/Array/Initializing an array of longs/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        final long[] longNumbers = new long[]{100000000001L, 100000000002L, 100000000003L};
+        System.out.print(Arrays.toString(longNumbers));
+    }
+}
